1e2e3328edecbfff97c97dae5280a522a6cf6431
Leena koo <leena.kevatkyla@gmail.com>
2024-11-09 12:41:03 +0200

    Lisää Balance-komponentin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
+import Balance from './components/Balance';
 import Lemon from './components/Lemon';
+
 
 function App() {
 
   return (
     <>
       <div>
+        <Balance total="157" />
         <Lemon />
       </div>  
     </>
