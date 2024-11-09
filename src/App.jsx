cd19650b505fd1457593e7a55e1e9ed73f516d08
Leena koo <leena.kevatkyla@gmail.com>
2024-11-09 16:11:38 +0200

    Lisää Menu-komponentin ja kääre-elementit

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,27 @@ import Balance from './components/Balance';
 import Booster from './components/Booster';
 import Header from './components/Header';
 import Lemon from './components/Lemon';
+import Menu from './components/Menu';
 
 
 function App() {
   const [clicks, setClicks] = useState(0);
   const handleClick = () => {
     setClicks(clicks + 1);  // kasvattaa napautusten määrää yhdellä
-  }
+    }
 
   return (
-    <>
-      <div>
-        <Header>lemon clicker</Header>
-        <Balance total={clicks} />
-        <Lemon onClick={handleClick} />
-        <Booster value = "3.2" />
-      </div>  
-    </>
+    <div className="root">
+      <div className="root_content">
+        <div className="container clicker">
+          <Header>lemon clicker</Header>
+          <Balance total={clicks} />
+          <Lemon onClick={handleClick} />
+          <Booster value = "3.2" />
+        </div>  
+      </div>
+      <Menu items={2} />
+    </div>
   )
 }
 
